b6880c573f4ec85ffba5db9456365912b1b3335e
Grady Robbins <gradyrobbins13@gmail.com>
2021-11-24 13:35:57 -0600

    replace {logo} with {qtr}

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import qtr from 'public/united-states-quarter-front.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={qtr} className="App-logo" alt="logo" />
           <p>
             Hello from Render! And from Grady
           </p>
-          
+
         </header>
       </div>
     );
